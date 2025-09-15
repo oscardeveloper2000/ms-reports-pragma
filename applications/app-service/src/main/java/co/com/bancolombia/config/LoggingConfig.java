cfcba28a93508a9495306d11e909abc9e047c586
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-09-15 00:45:54 -0500

    feat(logging): implement LoggerPort interface and SLF4J adapter for logging

---
+++ applications/app-service/src/main/java/co/com/bancolombia/config/LoggingConfig.java
@@ -0,0 +1,17 @@
+package co.com.bancolombia.config;
+
+
+
+import co.com.bancolombia.model.report.commom.LoggerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    public LoggerPort loggerPort() {
+        return new Slf4jLoggerAdapter();
+    }
+}
+
---
+++ applications/app-service/src/main/java/co/com/bancolombia/config/Slf4jLoggerAdapter.java
@@ -0,0 +1,19 @@
+package co.com.bancolombia.config;
+
+
+
+import co.com.bancolombia.model.report.commom.LoggerPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Slf4jLoggerAdapter implements LoggerPort {
+
+    private static final Logger logger = LoggerFactory.getLogger("application");
+
+    @Override public void info(String msg, Object... args) { if (logger.isInfoEnabled()) logger.info(msg, args); }
+    @Override public void warn(String msg, Object... args) { if (logger.isWarnEnabled()) logger.warn(msg, args); }
+    @Override public void debug(String msg, Object... args) { if (logger.isDebugEnabled()) logger.debug(msg, args); }
+    @Override public void error(String msg, Object... args) { if (logger.isErrorEnabled()) logger.error(msg, args); }
+    @Override public void error(String msg, Throwable t) { logger.error(msg, t); }
+}
