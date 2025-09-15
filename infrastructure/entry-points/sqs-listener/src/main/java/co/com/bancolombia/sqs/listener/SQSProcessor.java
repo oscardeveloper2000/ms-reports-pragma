4bd1e7132d7dd6b4e0dff4d55b03a6ebed346064
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-09-15 00:47:12 -0500

    feat(sqs): enhance SQSProcessor to process ApprovedLoan messages with logging

--- infrastructure/entry-points/sqs-listener/src/main/java/co/com/bancolombia/sqs/listener/SQSProcessor.java
+++ infrastructure/entry-points/sqs-listener/src/main/java/co/com/bancolombia/sqs/listener/SQSProcessor.java
@@ -1,5 +1,9 @@
 package co.com.bancolombia.sqs.listener;
 
+import co.com.bancolombia.model.report.ApprovedLoan;
+import co.com.bancolombia.model.report.commom.LoggerPort;
+import co.com.bancolombia.usecase.report.SaveApprovedLoan;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -11,11 +15,25 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class SQSProcessor implements Function<Message, Mono<Void>> {
     // private final MyUseCase myUseCase;
+    private final SaveApprovedLoan approvedLoanUseCase;
+    private final LoggerPort logger;
+    private static final Gson gson = new Gson();
 
-    @Override
-    public Mono<Void> apply(Message message) {
-        System.out.println(message.body());
-        return Mono.empty();
-        // return myUseCase.doAny(message.body());
+@Override
+public Mono<Void> apply(Message message) {
+    logger.info("SQSProcessor - Message received: {}", message.body());
+    try {
+        // Primer paso: deserializar el string JSON escapado
+        String jsonString = gson.fromJson(message.body(), String.class);
+
+        // Segundo paso: deserializar el JSON real al objeto
+        ApprovedLoan approvedLoan = gson.fromJson(jsonString, ApprovedLoan.class);
+
+        logger.info("SQSProcessor - Message mapped: {}", gson.toJson(approvedLoan));
+        return approvedLoanUseCase.apply(approvedLoan);
+    } catch (Exception e) {
+        logger.error("Error processing SQS message: {}", e.getMessage(), e);
+        return Mono.error(e);
     }
+}
 }
